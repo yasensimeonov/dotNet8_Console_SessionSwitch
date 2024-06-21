21fa4ba4211449241861cf0137977bdecc5f2e8b
Yasen Simeonov <y.simeonov@cnsys.bg>
2024-06-21 15:16:06 +0300

    Initial commit

---
+++ Console_SessionSwitch/Program.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Win32;
+
+SystemEvents.SessionSwitch += new SessionSwitchEventHandler(SystemEvents_SessionSwitch);
+
+Console.WriteLine("Monitoring SessionSwitch Events...Press Enter to quit.");
+Console.ReadLine();
+
+SystemEvents.SessionSwitch -= SystemEvents_SessionSwitch;
+Console.WriteLine("Monitoring stopped.");
+
+void SystemEvents_SessionSwitch(object sender, SessionSwitchEventArgs e)
+{
+    if (e.Reason == SessionSwitchReason.SessionLock)
+    {
+        //I left my desk
+        Console.WriteLine("Locked");
+    }
+    else if (e.Reason == SessionSwitchReason.SessionUnlock)
+    {
+        //I returned to my desk
+        Console.WriteLine("Unlocked");
+    }
+}
